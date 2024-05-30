d9c067c8ddda10e4a2be33d3246eb53a03394a52
ErickDiaz2001 <pablo.d3323@gmail.com>
2024-05-29 21:08:35 -0300

    Se agrega un Proyecto

---
+++ Core/Inc/itoa.h
@@ -0,0 +1,15 @@
+/*
+ * itoa.h
+ *
+ *  Created on: Apr 2, 2024
+ *      Author: Lenovo
+ */
+
+#ifndef INC_ITOA_H_
+#define INC_ITOA_H_
+
+#include "main.h"
+
+char* itoa(int value, char* result, int base);
+
+#endif /* INC_ITOA_H_ */
